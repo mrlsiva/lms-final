ee635952ed21c2f1995a0bbdca9b3f320ed12554
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2019-08-27 11:18:07 +0530

    added slash
    added slash

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 const corsOptions = {
-  origin: 'https://suguna.herokuapp.com',
+  origin: 'https://suguna.herokuapp.com/',
   optionsSuccessStatus: 200
 }
 
